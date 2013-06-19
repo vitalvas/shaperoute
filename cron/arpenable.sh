b9c10c49aee7f5dc5dc4fd3972e247174e972138
Vitaliy Vasilenko <vitalvas@MacBook-Pro-Vitaliy.local>
2013-06-20 02:23:56 +0300

    Удалил хлам

--- cron/arpenable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-
-arp -an | grep 'expires' | sed 's/(//' | sed 's/)//' | awk '{print "UPDATE users SET mac=\"" $4 "\" WHERE ip=\"" $2 "\" and mac=\"00:00:00:00:00:00\";"}' | mysql shaperoute
-
--- cron/clearstore.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-find /usr/shaperoute/ses_tmp -mtime +1 -delete
